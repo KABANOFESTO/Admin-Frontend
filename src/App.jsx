9fd3d9152533d18d5ab1d6bff8a6faf5762f4292
KABANOFESTO <kabanofesto1@gmail>
2024-08-04 19:00:13 +0200

    ch-proceed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signup from './Components/Signup';
+import Login from './Components/Login';
+import Reset from './Components/Reset';
 
 function App() {
   return (
     <>
-      
+      <Router>
+        <div className="App">
+          <Signup />
+          <div className="content">
+            <Routes>
+              <Route path='/login' element={<Login />} />
+              <Route path='/reset' element={<Reset />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
     </>
   )
 }
